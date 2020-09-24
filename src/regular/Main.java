c7e3099ec5f9c42fa835fafeb399260820f17028
Dzirt2006 <dzirt-2006@mail.ru>
2020-09-24 12:30:33 -0400

    primitive check

--- src/regular/Main.java
+++ src/regular/Main.java
@@ -1,5 +1,59 @@
 package regular;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+
+	}
+
+	void formater(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("================================");
+		for (int i = 0; i < 3; i++) {
+			String s1 = sc.next();
+			int x = sc.nextInt();
+			System.out.printf("%-15s%03d%n", s1, x); // format out 15 string and next 3 digits + \n by the end
+		}
+		System.out.println("================================");
+	}
+
+	void scan(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		double d = scan.nextDouble();
+		scan.nextLine(); // gets rid of the "\n"
+		String s = scan.nextLine();
+		scan.close();
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + i);
+
+	}
+
+	void isValueFit(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			try {
+				long x = sc.nextLong();
+				System.out.println(x + " can be fitted in:");
+				if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
+					System.out.println("* byte");
+				}
+				if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
+					System.out.println("* short");
+				}
+				if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+					System.out.println("* int");
+				}
+				if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) {
+					System.out.println("* long");
+				}
+			} catch (Exception e) {
+				System.out.println(sc.next() + " can't be fitted anywhere.");
+			}
+
+		}
+	}
 }
